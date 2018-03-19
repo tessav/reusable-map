4afe79d6b5c430f0ed6cadec423d71d4070755b1
tessav <tessavoon@gmail.com>
2018-03-20 04:33:30 +0800

    add layer filters

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 import buildingSVG from './assets/icons/marker-15.svg';
-import ReactMapGL from 'react-map-gl';
+import mapboxgl from 'mapbox-gl';
 import buildings from './assets/2017-south-asian-floods-boalkhali-chittagong-bangladesh_planet_osm_point_points.json';
 import landuse from './assets/2017-south-asian-floods-boalkhali-chittagong-bangladesh_planet_osm_polygon_polygons.json';
 import roads from './assets/2017-south-asian-floods-boalkhali-chittagong-bangladesh_planet_osm_line_lines.json';
-
+import clipbox from './assets/clipping_boundary.json';
 mapboxgl.accessToken = 'pk.eyJ1IjoidGVzc2F2MyIsImEiOiJjamRtcGpyOHUwMG13MzJwcW1wazN2a2dnIn0.aSD27NA7KzGc0p8PmhT4Sg';
 
 class App extends Component {
   map;
   popup;
+  state = {
+    buildings: true,
+    landuse: true,
+    roads: true,
+    keyInfo: true
+  }
 
   componentDidMount() {
-
     this.map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v9',
-      center: [91.9709773, 22.4085309],
-      zoom: 14
+      center: [91.9509773, 22.4285309],
+      zoom: 12
     });
     this.addIconImage('building', buildingSVG);
     this.popup = new mapboxgl.Popup({
@@ -27,20 +32,28 @@ class App extends Component {
     });
 
     this.map.on('load', () => {
+      const sources = [
+        {key: 'clipbox', data: clipbox},
+        {key: 'landuse', data: landuse},
+        {key: 'roads', data: roads},
+        {key: 'buildings', data: buildings}
+      ];
+      sources.map((s) => {
+        this.map.addSource(s.key, {
+          type: 'geojson',
+          data: s.data
+        });
+      });
 
-      this.map.addSource('buildings', {
-        type: 'geojson',
-        data: buildings
+      this.map.addLayer({
+        id: 'clipbox',
+        type: 'fill',
+        source: 'clipbox',
+        'paint': {
+          'fill-color': '#088',
+          'fill-opacity': 0.1
+        }
       });
-      this.map.addSource('landuse', {
-        type: 'geojson',
-        data: landuse
-      });
-      this.map.addSource('roads', {
-        type: 'geojson',
-        data: roads
-      });
-
       this.map.addLayer({
         id: 'landuse',
         type: 'fill',
@@ -50,7 +63,6 @@ class App extends Component {
           'fill-opacity': 0.8
         }
       });
-
       this.map.addLayer({
         id: 'roads',
         type: 'line',
@@ -64,7 +76,6 @@ class App extends Component {
             "line-width": 5
         }
       });
-
       this.map.addLayer({
         id: 'buildings',
         type: 'symbol',
@@ -80,25 +91,7 @@ class App extends Component {
       }, 'country-label-lg');
     });
     this.map.addControl(new mapboxgl.NavigationControl());
-    this.map.on('click', 'buildings', (e) => {
-      this.map.getCanvas().style.cursor = 'pointer';
-        console.log(e);
-        var coordinates = e.features[0].geometry.coordinates.slice();
-        var description = e.features[0].properties.description;
-
-        // Ensure that if the map is zoomed out such that multiple
-        // copies of the feature are visible, the popup appears
-        // over the copy being pointed to.
-        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-        }
-
-        // Populate the popup and set its coordinates
-        // based on the feature found.
-        this.popup.setLngLat(coordinates)
-            .setHTML(description)
-            .addTo(this.map);
-    });
+    this.map.on('click', 'buildings', this.updatePopup);
   }
 
   addIconImage(name, iconSVG) {
@@ -107,34 +100,65 @@ class App extends Component {
     img.src = iconSVG;
   }
 
+  updatePopup = (e) => {
+    this.map.getCanvas().style.cursor = 'pointer';
+      var coordinates = e.features[0].geometry.coordinates.slice();
+      var properties = e.features[0].properties;
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+      const presentProps = Object.keys(properties).filter((key) => (properties[key] && key !== 'z_index'));
+      let html = presentProps.map((key) => {
+          return key + " : " + properties[key] + "<br /> ";
+        });
+      this.popup.setLngLat(coordinates)
+          .setHTML(html.join(""))
+          .addTo(this.map);
+  }
+
+  handleCheckboxChange = (e) => {
+    this.setState({[e.target.value]: !this.state[e.target.value]});
+    this.map.setLayoutProperty(e.target.value, 'visibility',
+      e.target.checked ? 'visible' : 'none');
+  };
+
+  handleOverlayChange = (e) => {
+    this.setState({keyInfo: !this.state.keyInfo});
+  };
+
   render() {
     return (
       <div className='col12 contain'>
         <div className='quiet'><div id="map"></div></div>
-        <div className='map-overlay top'>
-      <div className='map-overlay-inner'>
-          <div id='legend' className='legend'>
+        {this.state.keyInfo ? (<div className='map-overlay top'>
+          <div className='map-overlay-inner'>
+            <div id='legend' className='legend'>
               <div className='bar'></div>
-              <div>Magnitude (m)</div>
+              <div>
+                <span className="area-stats">30%</span> Areas Covered<br />
+                <span className="features-stats">4</span> Features Collected
+              </div>
+            </div>
           </div>
-      </div>
-  </div>
-        <div className='col3 pad2 colored pin-left' style={{ height: '100vh'}}>
-          <h3 style={{ fontSize: '17px'}}>Field Data Map Tool</h3><br />
-          <hr style={{ backgroundColor: '#e7e7e7'}}/><br />
+        </div>) : (<span></span>)}
+        <div className='col3 pad2 colored pin-left stretch-height'>
+          <h3 className="title">Field Data Map Tool</h3>
+          <hr className="break" /><br />
           <h3>Filter by layers:</h3><br />
-          <input type="checkbox" value="Bike" /> Buildings<br/>
-          <input type="checkbox" value="Bike" /> Landuse<br/>
-          <input type="checkbox" value="Bike" /> Roads<br/>
+          <input type="checkbox" value="buildings"
+            checked={this.state.buildings}
+            onChange={this.handleCheckboxChange}/> Buildings<br/>
+          <input type="checkbox" value="landuse"
+            checked={this.state.landuse}
+            onChange={this.handleCheckboxChange} /> Landuse<br/>
+          <input type="checkbox" value="roads"
+            checked={this.state.roads}
+            onChange={this.handleCheckboxChange} /> Roads<br/>
           <br /><br />
-          <h3>Filter by features:</h3><br />
-          <input type="checkbox" value="Bike" /> Bike<br/>
-          <input type="checkbox" value="Bike" /> Bike<br/>
-          <input type="checkbox" value="Bike" /> Bike<br/>
-          <br /><br />
-          <h3>Display key information:</h3><br />
-          <input type="checkbox" value="Bike" /> % Mapped<br/>
-          <input type="checkbox" value="Bike" /> Feature attributes<br/>
+          <h3>Display:</h3><br />
+          <input type="checkbox" value="keyInfo"
+          checked={this.state.keyInfo}
+          onChange={this.handleOverlayChange} /> Key Info<br/>
         </div>
       </div>
     );
